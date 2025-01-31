452d93d788f0368f5a90264da0b208ff1bd82409
byrengar <216029059@stu.adu.edu.tr>
2025-01-31 13:20:52 +0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from app import app  # app.py'deki Flask uygulamasını içe aktar
-import os
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render.com'un beklediği port
-    app.run(host='0.0.0.0', port=port)
